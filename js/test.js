44bdb5b9c3ca88a87a85e346c5ad9b58f2c95267
amarnath <t.amarnath@hotmail.com>
2018-07-02 20:13:17 +0530

    array1

--- js/test.js
+++ js/test.js
@@ -1,4 +1,18 @@
+//array initialization from arrays
 console.log(Array.from(['123'], x => x + x ));
 
-
 console.log(Array.from('amar'));
+
+
+//concatination
+var alpha=["a","b","c"];
+var num = [1,2,3]
+var concata = alpha.concat(num);
+console.log(concata); 
+//unshift
+var ushift = alpha.unshift("a");
+console.log(alpha);
+//shift
+var shift = alpha.shift("a");
+console.log(alpha);
+
