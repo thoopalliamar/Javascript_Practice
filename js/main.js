afe50110546d9106c0f727c6391ee275aad5f6f0
amarnath <t.amarnath@hotmail.com>
2018-07-02 19:34:47 +0530

    arrays

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// var age = 15 ;
+// // var b = 3 ;
+// var text;
+// // c=a%b;
+// // document.getElementById("demo").innerHTML = c;
+// text = (age<20)?"too_young":"ok";
+// console.log()
+
+var number = 5;
+
+var r = myfunction(number);
+function myfunction(number){
+    var result = 1;
+    for(i=1;i<=number;i++)
+    {
+        result=result*i;
+    }
+    return result;
+}
+
+console.log(r);
+
+// var a = 5;
+
+
+// fucntion factorial(a);{
+//     for(i=1;i<=a;i++)
+//     {
+//         result=result*1;
+//     }
+//     return result;
+// }
+// var s = factorial(a);
+// console.log(s);
---
+++ js/test.js
@@ -0,0 +1,4 @@
+console.log(Array.from(['123'], x => x + x ));
+
+
+console.log(Array.from('amar'));
